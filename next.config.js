46eaea901a307136c7852e2227c2ba3c09612831
Henry <>
2024-06-06 22:36:28 +0100

    first commit

---
+++ next.config.js
@@ -0,0 +1,47 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig
+
+/*
+/!** @type {import('next').NextConfig} *!/
+const nextConfig = {
+    experimental: {
+        reactRoot: true,
+        serverComponentsExternalPackages: ["mongoose"],
+    },
+    images: {
+        domains: ['1h3.googleusercontent.com'],
+    },
+    webpack(config) {
+        config.experiments = {
+            ...config.experiments,
+            topLevelAwait: true,
+        }
+        return config
+    }
+}
+
+module.exports = nextConfig
+*/
